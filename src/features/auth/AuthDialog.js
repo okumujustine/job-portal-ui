42137559f686822e15584348f2177123f3e54237
okumujustine <okumujustine01@gmail.com>
2020-11-07 18:10:34 +0300

    resend activation code

--- src/features/auth/AuthDialog.js
+++ src/features/auth/AuthDialog.js
@@ -2,7 +2,6 @@ import * as React from "react";
 
 import { dialogStyle, dialogCloseButton } from "./AuthStyles";
 import { Link } from "react-router-dom";
-import { isError } from "lodash";
 
 export default function AuthDialog({
   content,
@@ -10,7 +9,12 @@ export default function AuthDialog({
   isOpen,
   isClose,
   isError,
+  isEmailInput,
+  onSubmitEmail,
+  isEmailLoading,
 }) {
+  const [emailInputState, setEmailInputState] = React.useState("");
+
   const openDialog = (
     <div
       style={dialogStyle}
@@ -35,6 +39,26 @@ export default function AuthDialog({
           go to login
         </Link>
       )}
+      {isEmailInput ? (
+        //okumujustine01@gmail.com
+        // Zila0781459239
+        <React.Fragment>
+          <form className="flex flex-col" onSubmit={onSubmitEmail}>
+            <input
+              type="email"
+              className="border-2 border-black mb-2 mt-2 py-1 px-4"
+              placeholder="enter email address here"
+              value={emailInputState}
+              onChange={(e) => setEmailInputState(e.target.value)}
+            />
+            <button type="submit" className="p-2 bg-black text-white">
+              {isEmailLoading
+                ? "sending code, wait..."
+                : "send verification code"}
+            </button>
+          </form>
+        </React.Fragment>
+      ) : null}
     </div>
   );
   return <React.Fragment>{isOpen ? openDialog : null}</React.Fragment>;
--- src/features/auth/Login.js
+++ src/features/auth/Login.js
@@ -4,19 +4,37 @@ import { Redirect } from "react-router-dom";
 import { useAlert } from "react-alert";
 
 import { loginUser } from "../../redux/actions/auth/AuthAction";
+import AuthDialog from "./AuthDialog";
 import "./Auth.css";
+import axios from "axios";
+import { baseUrl } from "../common/constants";
+import { config } from "../../helperfuncs/token";
 
 function Login({ authState, loginUser }) {
   const { isAuthenticated, loginFailedError } = authState;
 
   const [email, setEmail] = React.useState("");
   const [password, setPassword] = React.useState("");
+  const [openAuthDialog, setOpenAuthDialog] = React.useState(false);
+  const [dialogMessage, setDialogMessage] = React.useState("");
+  const [isError, setIsError] = React.useState(false);
+  const [isEmailInput, setIsEmailInput] = React.useState(false);
+  const [isEmailLoading, setIsEmailLoading] = React.useState(false);
 
   const alert = useAlert();
 
   React.useEffect(() => {
+    if (loginFailedError && loginFailedError === "Email is not verified") {
+      setIsError(true);
+      setIsEmailInput(true);
+      setDialogMessage("email is not verfied, send a new verification link");
+      setOpenAuthDialog(true);
+      return;
+    }
+
     if (loginFailedError) {
       alert.error(loginFailedError);
+      return;
     }
   }, [authState]);
 
@@ -29,35 +47,70 @@ function Login({ authState, loginUser }) {
     loginUser({ email, password });
   };
 
-  return (
-    <form
-      onSubmit={onLoginUser}
-      className="auth-form flex flex-col w-5/12 m-auto"
-    >
-      <h1 className="font-bold self-center uppercase">Login Here</h1>
-      <div className="flex flex-col mt-6">
-        <label>Email Address:</label>
-        <input
-          className="auth-form-input"
-          placeholder="email address"
-          onChange={(e) => setEmail(e.target.value)}
-        />
-      </div>
+  const closeAuthDialog = () => {
+    setOpenAuthDialog(false);
+  };
 
-      <div className="flex flex-col mt-6">
-        <label>Password</label>
-        <input
-          type="password"
-          className="auth-form-input"
-          placeholder="password"
-          password="password"
-          onChange={(e) => setPassword(e.target.value)}
-        />
-      </div>
-      <button className="mt-6 auth-button" type="submit">
-        login
-      </button>
-    </form>
+  const onSubmitEmail = (e) => {
+    e.preventDefault();
+    setIsEmailLoading(true);
+    const userEmail = e.target[0].value;
+
+    axios
+      .post(`${baseUrl}/auth/re-verify/`, { email: userEmail }, config)
+      .then((res) => {
+        alert.success("Activation email sent");
+        setIsEmailLoading(false);
+        setOpenAuthDialog(false);
+      })
+      .catch((error) => {
+        setIsEmailLoading(false);
+        setOpenAuthDialog(false);
+        alert.error(error.response.data);
+      });
+  };
+
+  return (
+    <React.Fragment>
+      <AuthDialog
+        isOpen={openAuthDialog}
+        isClose={closeAuthDialog}
+        isError={isError}
+        content={dialogMessage}
+        isEmailInput={isEmailInput}
+        isEmailLoading={isEmailLoading}
+        onSubmitEmail={onSubmitEmail}
+      />
+
+      <form
+        onSubmit={onLoginUser}
+        className="auth-form flex flex-col w-5/12 m-auto"
+      >
+        <h1 className="font-bold self-center uppercase">Login Here</h1>
+        <div className="flex flex-col mt-6">
+          <label>Email Address:</label>
+          <input
+            className="auth-form-input"
+            placeholder="email address"
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </div>
+
+        <div className="flex flex-col mt-6">
+          <label>Password</label>
+          <input
+            type="password"
+            className="auth-form-input"
+            placeholder="password"
+            password="password"
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </div>
+        <button className="mt-6 auth-button" type="submit">
+          login
+        </button>
+      </form>
+    </React.Fragment>
   );
 }
 
