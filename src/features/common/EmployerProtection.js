3dfe17d5a8623b7d4a5a82cf40ce2e49e5c791e2
okumujustine <okumujustine01@gmail.com>
2021-02-16 10:11:02 +0300

    log in as admin error

--- src/features/common/EmployerProtection.js
+++ src/features/common/EmployerProtection.js
@@ -1,18 +1,11 @@
 import React from "react";
 import { Route, Redirect } from "react-router-dom";
 import { connect } from "react-redux";
-import { useAlert } from "react-alert";
 import { authTokenKey, userRoleKey } from "../common/constants";
 
 const EmployerProtection = ({ component: Component, authState, ...rest }) => {
   const authToken = localStorage.getItem(authTokenKey);
   const userRole = localStorage.getItem(userRoleKey);
-
-  const alert = useAlert();
-
-  if (!authToken && !userRole) {
-    alert.error("loggin as admin to access this route");
-  }
 
   return (
     <Route
